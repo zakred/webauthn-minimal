060206a5a5571d5c0c3de0cffa1df7098f634a2d
Eduardo C <eduardo.cavazos1@wipro.com>
2023-08-07 22:55:14 -0600

    add format to login server

--- login-service/src/wso2-service.js
+++ login-service/src/wso2-service.js
@@ -5,7 +5,7 @@ const url = require("url");
  * Service to interact with WSO2 Identity server.
  */
 class Wso2Service {
-    SCOPE = 'openid'
+    SCOPE = "openid";
 
     constructor(wso2Fqdn, redirectCallback, clientId, clientSecret) {
         this.wso2Fqdn = wso2Fqdn;
@@ -13,19 +13,21 @@ class Wso2Service {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
 
-        this.WSO2_AUTH_URL = this.wso2Fqdn + '/oauth2/authorize';
-        this.WSO2_COMMON_AUTH_URL = this.wso2Fqdn + '/commonauth';
-        this.WSO2_TOKEN_URL = this.wso2Fqdn + '/oauth2/token';
+        this.WSO2_AUTH_URL = this.wso2Fqdn + "/oauth2/authorize";
+        this.WSO2_COMMON_AUTH_URL = this.wso2Fqdn + "/commonauth";
+        this.WSO2_TOKEN_URL = this.wso2Fqdn + "/oauth2/token";
     }
 
-    base64UrlToBase64 = (base64Url) => base64Url.replace(/\-/g, '+').replace(/_/g, '/') + '='.repeat((base64Url.length % 4) ? 4 - (base64Url.length % 4) : 0);
+    base64UrlToBase64 = (base64Url) =>
+        base64Url.replace(/\-/g, "+").replace(/_/g, "/") +
+        "=".repeat(base64Url.length % 4 ? 4 - (base64Url.length % 4) : 0);
     #handleHttpError(e) {
         if (e.response) {
-            console.log(`Status: ${e.response.status}`)
-            console.log(`Body: ${JSON.stringify(e.response.data, 0, 4)}`)
+            console.log(`Status: ${e.response.status}`);
+            console.log(`Body: ${JSON.stringify(e.response.data, 0, 4)}`);
         }
         const ret = {
-            message: e.message ?? 'Error',
+            message: e.message ?? "Error",
         };
         if (e.response && e.response.data) {
             ret.data = e.response.data;
@@ -38,35 +40,41 @@ class Wso2Service {
      *
      * @returns {Promise<{message: string}|any>} payload with challenge
      */
-    startAuthentication = async() => {
+    startAuthentication = async () => {
         const formData = new URLSearchParams();
-        formData.append('response_type', 'code');
-        formData.append('client_id', this.clientId);
-        formData.append('redirect_uri', this.redirectCallback);
-        formData.append('nonce', 'asd');
-        formData.append('scope', this.SCOPE);
+        formData.append("response_type", "code");
+        formData.append("client_id", this.clientId);
+        formData.append("redirect_uri", this.redirectCallback);
+        formData.append("nonce", "asd");
+        formData.append("scope", this.SCOPE);
 
         const config = {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+                "Content-Type": "application/x-www-form-urlencoded",
             },
-            maxRedirects: 0
+            maxRedirects: 0,
         };
 
         try {
-            return await axios.post(this.WSO2_AUTH_URL, formData,  config);
+            return await axios.post(this.WSO2_AUTH_URL, formData, config);
         } catch (e) {
             const resp = e.response;
             if (resp.status === 302) {
-                const queryParams = url.parse(resp.headers.location, true).query;
+                const queryParams = url.parse(
+                    resp.headers.location,
+                    true,
+                ).query;
                 const data = JSON.parse(queryParams.data);
-                data.sessionDataKey = queryParams.sessionDataKey
-                data.publicKeyCredentialRequestOptions.challenge = this.base64UrlToBase64(data.publicKeyCredentialRequestOptions.challenge);
+                data.sessionDataKey = queryParams.sessionDataKey;
+                data.publicKeyCredentialRequestOptions.challenge =
+                    this.base64UrlToBase64(
+                        data.publicKeyCredentialRequestOptions.challenge,
+                    );
                 return data;
             }
             return this.#handleHttpError(e);
         }
-    }
+    };
 
     /**
      * Calls the FIDO authenticator with the signed challenge.
@@ -77,22 +85,26 @@ class Wso2Service {
     commonAuth = async (req) => {
         console.log(req);
         const formData = new URLSearchParams();
-        formData.append('sessionDataKey', req.sessionDataKey);
-        formData.append('tokenResponse', JSON.stringify(req.tokenResponse));
+        formData.append("sessionDataKey", req.sessionDataKey);
+        formData.append("tokenResponse", JSON.stringify(req.tokenResponse));
 
         const config = {
             maxRedirects: 0,
             auth: {
                 username: this.clientId,
-                password: this.clientSecret      
+                password: this.clientSecret,
             },
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
         };
 
         try {
-            return await axios.post(this.WSO2_COMMON_AUTH_URL, formData,  config);
+            return await axios.post(
+                this.WSO2_COMMON_AUTH_URL,
+                formData,
+                config,
+            );
         } catch (e) {
             const resp = e.response;
             if (resp.status === 302) {
@@ -100,7 +112,7 @@ class Wso2Service {
             }
             return this.#handleHttpError(e);
         }
-    }
+    };
 
     /**
      * Follows the redirection URI to retrieve the authorization code.
@@ -113,11 +125,11 @@ class Wso2Service {
             maxRedirects: 0,
             auth: {
                 username: this.clientId,
-                password: this.clientSecret      
+                password: this.clientSecret,
             },
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
         };
 
         try {
@@ -125,12 +137,15 @@ class Wso2Service {
         } catch (e) {
             const resp = e.response;
             if (resp.status === 302) {
-                const queryParams = url.parse(resp.headers.location, true).query;
+                const queryParams = url.parse(
+                    resp.headers.location,
+                    true,
+                ).query;
                 return queryParams.code;
             }
             return this.#handleHttpError(e);
         }
-    }
+    };
 
     /**
      * Get the tokens using the authorized code.
@@ -140,27 +155,31 @@ class Wso2Service {
      */
     getTokens = async (authorization_code) => {
         const formData = new URLSearchParams();
-        formData.append('grant_type', 'authorization_code');
-        formData.append('redirect_uri', this.redirectCallback);
-        formData.append('code', authorization_code);
+        formData.append("grant_type", "authorization_code");
+        formData.append("redirect_uri", this.redirectCallback);
+        formData.append("code", authorization_code);
 
         const config = {
             auth: {
                 username: this.clientId,
-                password: this.clientSecret      
+                password: this.clientSecret,
             },
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
         };
 
         try {
-            const resp = await axios.post(this.WSO2_TOKEN_URL, formData,  config);
+            const resp = await axios.post(
+                this.WSO2_TOKEN_URL,
+                formData,
+                config,
+            );
             return resp.data;
         } catch (e) {
             return this.#handleHttpError(e);
         }
-    }
+    };
 }
 
-module.exports = Wso2Service
+module.exports = Wso2Service;
